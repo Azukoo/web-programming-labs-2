5f3f7e7926019a2ff32341298fa54fe7955bb36d
Санданова Виктория <viktoria.near@gmail.com>
2024-10-15 22:54:54 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.errorhandler(404)
@@ -379,7 +379,10 @@ def add_flower(name):
     <p>Полный список: {flower_list}</p>
     </body>
 </html>
-'''    
+'''  
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
    
 
 
