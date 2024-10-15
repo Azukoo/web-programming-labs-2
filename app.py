f267365726e368e74e557cc7ac822e684fb69e86
Санданова Виктория <viktoria.near@gmail.com>
2024-10-15 21:05:25 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -357,3 +357,15 @@ def a():
 def a2():
     return 'со слэшем'
 
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        return "Упс! Такого цветка нет :(", 404
+    else:
+        return "цветок: " + flower_list[flower_id]
+
+
+
+
